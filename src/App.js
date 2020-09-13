8c1a002e1859be1ed0e22d3d8415ca45a625fced
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2020-09-14 02:14:39 +0530

    on game over reset snake

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Food from './components/Food'
 import './styles/App.scss';
 
 class App extends Component {
+	
 	constructor(props) {
 		super(props)
 
@@ -11,11 +12,11 @@ class App extends Component {
 			food: this.getRandomCoordinates(),
 			speed: 200,
 			direction: 'RIGHT',
+			move: true,
 			snakeDots : [
 				[0,0],
 				[2,0],
-				[4,0],
-				[6,0]
+				[4,0]
 			]
 		}
 	}
@@ -30,30 +31,37 @@ class App extends Component {
 	}
 
 	moveSnake = () => {
-		const { snakeDots, direction } = this.state
+		const { snakeDots, direction, move } = this.state
 
-		let dots = [...snakeDots]
-		let head = dots[dots.length - 1]
+		if(move) {
 
-		switch(direction) {
-			case 'LEFT':
-				head = [head[0]-2, head[1]]
-				break
-			case 'UP':
-				head = [head[0], head[1]-2]
-				break
-			case 'RIGHT':
-				head = [head[0]+2, head[1]]
-				break
-			case 'DOWN':
-				head = [head[0], head[1]+2]
-				break
+			let dots = [...snakeDots]
+			let head = dots[dots.length - 1]
+
+			switch(direction) {
+				case 'LEFT':
+					head = [head[0]-2, head[1]]
+					break
+				case 'UP':
+					head = [head[0], head[1]-2]
+					break
+				case 'RIGHT':
+					head = [head[0]+2, head[1]]
+					break
+				case 'DOWN':
+					head = [head[0], head[1]+2]
+					break
+			}
+
+			dots.push(head)
+			dots.shift()
+
+			this.setState({ snakeDots: dots })
 		}
+	}
 
-		dots.push(head)
-		dots.shift()
-
-		this.setState({ snakeDots: dots })
+	componentDidUpdate() {
+		this.checkIfOutofBounds();
 	}
 
 	handleKeyDown = e => {
@@ -72,7 +80,35 @@ class App extends Component {
 			case 40:
 				this.setState({ direction: 'DOWN' })
 				break
+			case 32:
+				this.setState({ move: !this.state.move })
 		}
+	}
+
+	checkIfOutofBounds = () => {
+		const { snakeDots } = this.state
+		let head = snakeDots[snakeDots.length-1]
+		if(head[0] > 100 || head[0] < 0 || head[1] >100 || head[1]<0) {
+			this.handleGameOver()
+		}
+	}
+
+	handleGameOver = () => {
+		this.setState( (state, props) => {
+			return {
+				food: this.getRandomCoordinates(),
+				speed: 2000,
+				direction: 'RIGHT',
+				snakeDots : [
+					[0,0],
+					[2,0],
+					[4,0]
+				]
+			}
+		})
+		console.log(this.state)
+		// alert(`Game over dude. Your score is ${this.state.snakeDots.length - 3}`)
+		console.log(`Game over dude. Your score is ${this.state.snakeDots.length - 3}`)
 	}
 
 	componentDidMount() {
@@ -82,10 +118,10 @@ class App extends Component {
 	}
 
     render() {
-    	const {snakeDots, food} = this.state
+    	const {snakeDots, food, test} = this.state
         return (
             <div className="game-area">
-            	<Snake snakeDots={ snakeDots } />
+            	<Snake snakeDots={ snakeDots ? snakeDots : test } />
             	<Food dot={food}/>
             </div>
         );
